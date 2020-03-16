fdef6f3de66c214660db59d5285b2ef3558d8ed3
amunwe-ene <eneamunwe@gmail.com>
2020-03-16 19:28:57 +0200

    fixed google sign in

--- SignIn.js
+++ SignIn.js
@@ -10,6 +10,7 @@ import { LoginManager,AccessToken } from 'react-native-fbsdk';
 
 export default class SignIn extends React.Component {
   state = { email: '', password: '', errorMessage: null,isLoading:false }
+<<<<<<< Updated upstream
 
  
   handleGoogleSignIn = async () => {
@@ -26,6 +27,56 @@ export default class SignIn extends React.Component {
       this.props.navigation.navigate('Home')
     } 
       }
+=======
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      if(user){
+        this.props.navigation.navigate('Home')
+      }
+    })
+  }
+ 
+  handleGoogleSignIn = async () => {
+   
+     
+      this.setState({isLoading:true})
+      try{
+        await GoogleSignin.configure({
+          scopes: ['https://www.googleapis.com/auth/contacts.readonly','https://www.googleapis.com/auth/androidpublisher', 'profile','email'],
+          webClientId: '1045040137199-hktsot474fqlafsa7jje7d1k84nkdq44.apps.googleusercontent.com' // required
+        });
+      }catch(e){
+        this.setState({errorMessage: `1; ${e}`})
+        this.setState({isLoading:false})
+      }
+        //  GoogleSignin.signIn().then(cred =>{
+        //  const { accessToken, _idToken } = cred
+        //  return {accessToken, _idToken}
+        //  }).catch(e=>{
+        //   this.setState({isLoading:false})
+        //   this.setState({ errorMessage: `1 ${e.message}` })
+        //  })
+        const {accessToken, idToken} = await GoogleSignin.signIn()
+    const credential = firebase.auth.GoogleAuthProvider.credential(idToken, accessToken);
+   
+      firebase.auth().signInWithCredential(credential).then(()=>{
+        this.setState({isLoading:false})
+        this.setState({ errorMessage: null})
+      this.props.navigation.navigate('Home')
+      }).catch(e=>{
+        this.setState({isLoading:false})
+        this.setState({ errorMessage: `Code: ${e.code} - Message: ${e.message}` })
+     
+      });
+    // }catch(e){
+    //   this.setState({errorMessage: `3: ${e}`})
+    //   this.setState({isLoading:false})
+    // }
+    
+    
+
+  }
+>>>>>>> Stashed changes
     
   handleFacebookSignIn = async ()=>{
     this.setState({isLoading:true})
@@ -33,25 +84,45 @@ export default class SignIn extends React.Component {
     const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
     
     if (result.isCancelled) {
+<<<<<<< Updated upstream
       throw new Error('User cancelled the login process');
       this.setState({ errorMessage: 'User cancelled the login process' })
+=======
+      this.setState({ errorMessage: 'User cancelled the login process' })
+      
+      this.setState({isLoading:false})
+      throw new Error('User cancelled the login process');
+      
+>>>>>>> Stashed changes
     }
     
     const data = await AccessToken.getCurrentAccessToken();
     
     if (!data) {
+<<<<<<< Updated upstream
       throw new Error('Something went wrong obtaining access token');
       this.setState({ errorMessage: 'Something went wrong obtaining access token' })
+=======
+      this.setState({ errorMessage: 'Something went wrong obtaining access token' })
+      this.setState({isLoading:false})
+      throw new Error('Something went wrong obtaining access token');
+      
+>>>>>>> Stashed changes
     }
     const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
     const {currentUser} = await firebase.auth().signInWithCredential(credential);
     this.setState({isLoading:false})
     if(currentUser){
+<<<<<<< Updated upstream
+=======
+      this.setState({ errorMessage: null })
+>>>>>>> Stashed changes
       this.props.navigation.navigate('Home')
     }
   }
 
   handleLogin = () => {
+<<<<<<< Updated upstream
       this.setState({isLoading:true})
     const { email, password } = this.state
     firebase
@@ -62,6 +133,22 @@ export default class SignIn extends React.Component {
     })
       .catch(error => {this.setState({ errorMessage: error.message })
       this.setState({isLoading:false})})
+=======
+      
+    const { email, password } = this.state
+    if(!email && !password){this.setState({ errorMessage: 'Email or Pasword field is empty' })}else{
+      this.setState({isLoading:true})
+      firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ errorMessage: null })
+        this.props.navigation.navigate('Home')
+      this.setState({isLoading: false})
+    })
+      .catch(error => {this.setState({ errorMessage: error.message })
+      this.setState({isLoading:false})})}
+>>>>>>> Stashed changes
   }
 
   render() {
